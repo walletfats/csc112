878ae797baa3a64cc196f9c4aec99067f1ab0e3a
Jason <jasonste123@gmail.com>
2020-02-16 14:24:59 -0500

    Driver now calls all toString() 's

--- Driver.java
+++ Driver.java
@@ -14,8 +14,9 @@ public class Driver
     public static void main(String[] args)
     {
         IPod bar = new IPod("apple", 100, 5, "double a", "iphone 2", 4);
-        
-        
+        CellPhone f = new CellPhone("apple", 5, 5, "222", true, "AA");
+
+        System.out.print(f);
         System.out.println(bar);
     }
 }
