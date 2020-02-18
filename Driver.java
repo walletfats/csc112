a21c4610a27322ea7febcd1e51e9a6b8c1c38249
Jason <43427061+walletfats@users.noreply.github.com>
2020-02-18 16:15:49 -0500

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -13,8 +13,8 @@ public class Driver
 {
     public static void main(String[] args)
     {
-        IPod bar = new IPod("apple", 100, 5, "double a", "iphone 2", 4);
-        CellPhone f = new CellPhone("apple", 5, 5, "222", true, "AA");
+        IPod ipodTouch = new IPod("apple", 100, 5, "AA", "ipod 2", 4);
+        CellPhone iphone = new CellPhone("apple", 5, 5, "222", true, "AA");
         AlarmClock loud = new AlarmClock("occ", 5, 3, "bedroom", "1:50PM", "1:51PM");
         Clock louder = new Clock("occ", 5, 3, "kitchen", "2:50PM");
         Electronics computer = new Electronics("dell", 2, 10);
@@ -22,14 +22,22 @@ public class Driver
         PortableElectronics phone = new PortableElectronics("apple", 50000, 8, "AAA");
         WallClock foo = new WallClock("unknown", 50, 8, "bathroom", "2:14PM");
         Printer print = new Printer("HP", 30, 5, "computer room", 900, true, true);
-
-        System.out.println(foo);
-        System.out.println(print);
-        System.out.println(loud);
-        System.out.println(louder);
-        System.out.println(computer);
-        System.out.println(washerMachine);
-        System.out.println(f);
-        System.out.println(bar);
+        
+        System.out.println("IPod\n" + ipodTouch);         //Ipod
+        System.out.print("\n\n");
+        System.out.println("Cell phone\n" + iphone);      //cell phone 
+        System.out.print("\n\n");
+        System.out.println("Alarm Clock\n" + loud);       //alarm clock
+        System.out.print("\n\n");
+        System.out.println("Clock\n" + louder);           //clock
+        System.out.print("\n\n");
+        System.out.println("Electronics\n" + computer);            //electronics
+        System.out.print("\n\n");
+        System.out.println("Home Appliance\n" + washerMachine);    //home appliance
+        System.out.print("\n\n");
+        System.out.println(foo);        //wall clock
+        System.out.print("\n\n");
+        System.out.println(print);      //printer 
+        System.out.print("\n\n");
     }
 }
